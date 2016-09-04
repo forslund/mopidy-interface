a8a6d05715e1a347c1e1d1027260343776c3021d
Åke Forslund <ake.forslund@gmail.com>
2016-09-04 20:14:46 +0200

    Add mopidy POST interface

---
+++ mopidy_interface/__init__.py
@@ -0,0 +1,1 @@
+from . import mopidy_post
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='mopidy_interface',
+      version='0.1.dev',
+      packages = ['mopidy_interface',],
+      description='Python bindings for the mopidy post API',
+      author='Åke Forslund',
+      author_email='ake.forslund@gmail.com',
+      url='https://github.com/forslund/mopidypost',
+      download_url='https://github.com/forslund/mopidypost/tarball/0.1',
+      keywords=['mopidy', 'post interface'],
+      )
